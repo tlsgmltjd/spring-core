08ede19d31d0ef4169d8b3237bd71aebfc81285a
신희성 <vvwv3092@gmail.com>
2024-02-26 22:03:00 +0900

    Add fodun 2 benas error solve

--- src/main/java/hello/core/discount/RateDiscountPolicy.java
+++ src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -2,9 +2,13 @@ package hello.core.discount;
 
 import hello.core.member.Grade;
 import hello.core.member.Member;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
+//@Qualifier("mainDiscountPolicy") // 추가 구분자 설정 (빈 이름을 변경하는 것이 아니다.)
+@Primary
 public class RateDiscountPolicy implements DiscountPolicy {
 
     private int discountPercent = 10;
--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -5,10 +5,11 @@ import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor // 필수값 생성자
+//@RequiredArgsConstructor // 필수값 생성자
 public class OrderServiceImpl implements OrderService {
 
     // 인터페이스에만 의존하게 DIP를 준수했다.
@@ -17,11 +18,24 @@ public class OrderServiceImpl implements OrderService {
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
 
-//    @Autowired
-//    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-//        this.memberRepository = memberRepository;
-//        this.discountPolicy = discountPolicy;
-//    } // == @RequiredArgsConstructor 롬복이 어노테이션 프로세서라는 기능을 활용하여 컴파일 시점에 생성자 코드를 자동으로 생성해준다
+    // 7-5 조회할 빈이 2개 이상인 문제
+    // @Autowired는 타입으로 조회한다. ac.getBean(DiscountPolicy.class)
+    // -> 선택된 빈이 2개 이상일때 문제가 발생한다. Fix, Rate
+    // 하위 타입으로 조회가 가능하지만 DIP위배, 유연성 떨어짐
+
+    // 해결 방법
+    // @Autowired 필드명 매칭
+    // - 여러 빈이 있으면 파라미터의 이름으로 빈 이름을 추가 매칭한다. @Autowired (..., DiscountPolicy rateDiscountPolicy)
+    // @Quilfier -> @Quilfier끼리 매칭 -> 빈 이름 매칭
+    // - 빈에 붙은 @Quilfier와 주입할때 붙은 @Quilfier가 같은 빈을 주입시킨다. 못 찾았다면 @Quilfier의 이름의 빈을 추가로 찾는다.
+    // @Primary 사용
+    // - 주입하려는 타입의 여러 빈이 있다면 @Primary가 붙은 빈이 우선으로 주입됨
+    // @Primary는 기본값처럼 동작, @Quilfier는 상세하게 동작한다. 넓은 범위보다 좁은 범위의 선택권이 우선순위가 더 높다. @Quilfier가 우선권이 더 높음
+    @Autowired
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    } // == @RequiredArgsConstructor 롬복이 어노테이션 프로세서라는 기능을 활용하여 컴파일 시점에 생성자 코드를 자동으로 생성해준다
 
     // 생성자 주입
     // 불변, 필수 의존관계
